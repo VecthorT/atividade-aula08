66708fa510f1fab4125ea58605879c99b7d9b292
vecthort <vecthorteixeira@gmail.com>
2024-09-19 22:45:09 -0400

    Implementado o calculo do Fatorial 0

--- calculoFatorial.py
+++ calculoFatorial.py
@@ -2,11 +2,14 @@ def calculadoraFatorial(numFat):
     numPrincipal = numFat;
     calcFat = numFat;
     cont = 1;
-    while cont < numFat:
-        calcFat *= numPrincipal - 1;
-        numPrincipal -= 1;
-        cont += 1;   
-    return calcFat
+    if numFat == 0:
+        return 1
+    else:
+        while cont < numFat:
+            calcFat *= numPrincipal - 1;
+            numPrincipal -= 1;
+            cont += 1;   
+        return calcFat
 
-print(calculadoraFatorial(12))
+print(calculadoraFatorial(0))
 
